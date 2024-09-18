3917fe86d61a75d160f7e955747db387fa856e2d
BrunoPoianoVlcode <bruno.poiano@vlcode.com.br>
2024-09-18 15:28:01 -0300

    Updating lsp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,7 +12,6 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      -- Set up nvim-cmp.
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
@@ -30,7 +29,7 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),  
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,95 +1,72 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"biome",
-					"jsonls",
-					"eslint",
-					"html",
-					"lwc_ls",
-					"ast_grep",
-					"cssls",
-					"css_variables",
-					"cssmodules_ls",
-					"tailwindcss",
-					"unocss",
-					"intelephense",
-					"psalm",
-					"emmet_language_server",
-					"emmet_ls",
-					"rnix",
-					"volar",
-					"vuels",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.biome.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lwc_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ast_grep.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.css_variables.setup({ capabilities = capabilities })
-			lspconfig.cssmodules_ls.setup({ capabilities = capabilities })
-			lspconfig.tailwindcss.setup({ capabilities = capabilities })
-			lspconfig.unocss.setup({ capabilities = capabilities })
-			lspconfig.intelephense.setup({ capabilities = capabilities })
-			lspconfig.psalm.setup({ capabilities = capabilities })
-			lspconfig.emmet_language_server.setup({ capabilities = capabilities })
-			lspconfig.emmet_ls.setup({ capabilities = capabilities })
-			lspconfig.rnix.setup({ capabilities = capabilities })
-			lspconfig.volar.setup({ capabilities = capabilities })
-			lspconfig.vuels.setup({ capabilities = capabilities })
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities, })
+      lspconfig.biome.setup({ capabilities = capabilities, })
+      lspconfig.jsonls.setup({ capabilities = capabilities, })
+      lspconfig.eslint.setup({ capabilities = capabilities, })
+      lspconfig.html.setup({ capabilities = capabilities, })
+      lspconfig.lwc_ls.setup({ capabilities = capabilities, })
+      lspconfig.ast_grep.setup({ capabilities = capabilities, })
+      lspconfig.cssls.setup({ capabilities = capabilities, })
+      lspconfig.css_variables.setup({ capabilities = capabilities })
+      lspconfig.cssmodules_ls.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+      lspconfig.unocss.setup({ capabilities = capabilities })
+      lspconfig.intelephense.setup({ capabilities = capabilities })
+      lspconfig.psalm.setup({ capabilities = capabilities })
+      lspconfig.emmet_language_server.setup({ capabilities = capabilities })
+      lspconfig.rnix.setup({ capabilities = capabilities })
+      lspconfig.volar.setup({ capabilities = capabilities })
+      lspconfig.vuels.setup({ capabilities = capabilities })
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, {})
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, {})
-			vim.keymap.set("n", "<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, {})
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, {})
-		end,
-	},
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = function(_, bufnr)
+          -- Disable formatting to avoid conflicts with prettier or eslint
+          vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics, {
+              underline = true,
+              virtual_text = false,
+              update_in_insert = true,
+            }
+          )
+        end,
+      })
+
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, {})
+      vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, {})
+      vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, {})
+      vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<space>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, {})
+    end,
+  },
 }
