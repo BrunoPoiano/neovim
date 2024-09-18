ea4c9c193659d4009dd3c7572bab90b5e7bbcd75
BrunoPoianoVlcode <bruno.poiano@vlcode.com.br>
2024-09-18 12:40:41 -0300

    first commit

---
+++ init.lua
@@ -0,0 +1,54 @@
+local global = vim.g
+local o = vim.o
+
+vim.scriptencoding = "utf-8"
+
+-- Map <leader>
+global.mapleader = " "
+global.maplocalleader = " "
+
+-- Editor options
+o.number = true -- Print the line number in front of each line
+o.relativenumber = false -- Show the line number relative to the line with the cursor in front of each line.
+o.clipboard = "unnamedplus" -- Uses the clipboard register for all operations except yank.
+o.syntax = "on" -- Syntax highlighting
+o.autoindent = true -- Copy indent from current line when starting a new line.
+o.cursorline = true -- Highlight the screen line of the cursor with CursorLine.
+o.expandtab = true -- Use spaces instead of tabs
+o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent.
+o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
+o.encoding = "utf-8" -- Character encoding used inside Vim.
+o.fileencoding = "utf-8" -- Character encoding for the file of this buffer.
+o.ruler = true -- Show the line and column number of the cursor position.
+o.mouse = "a" -- Enable mouse in all modes
+o.title = true -- Set the window title to the value of 'titlestring'
+o.hidden = true -- Buffer becomes hidden when abandoned
+o.ttimeoutlen = 0 -- Time in milliseconds for key code or mapped key JKnce to complete
+o.wildmenu = true -- Enhanced command-line completion
+o.showcmd = true -- Show (partial) command in the last line of the screen
+o.showmatch = true -- Briefly jump to the matching bracket
+o.inccommand = "split" -- Show effects of substitute commands as you type
+o.splitbelow = true -- New horizontal splits appear below the current window
+o.splitright = true -- New vertical splits appear to the right of the current window
+o.softtabstop = 2
+
+require("config.lazy")
+
+vim.o.clipboard = "unnamedplus"
+
+vim.api.nvim_set_keymap("n", "<C-u>", "u", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.js", "*.ts", "*.py", "*.c", "*.cpp", "*.tsx", "*.jsx" },
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+})
+
+vim.api.nvim_set_keymap("n", "gt", ":tabnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gT", ":tabprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-w>", ":tabclose<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<S-k>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-j>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-x>", "dd", { noremap = true, silent = true })
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree close<CR>', {})
+    require("neo-tree").setup({
+      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+      }
+    })
+  end
+}
