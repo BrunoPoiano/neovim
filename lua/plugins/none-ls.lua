d47d6a90003a6b767ff2b38a055e44ae22e157c1
BrunoPoiano <bruno.poiano@proton.me>
2024-09-19 21:21:48 -0300

    Adding git plugins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,11 +9,17 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-       -- null_ls.builtins.diagnostics.stylua,
-       --: null_ls.builtins.completion.stylua,
+        -- null_ls.builtins.diagnostics.stylua,
+        --: null_ls.builtins.completion.stylua,
 
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.phpcsfixer.with({
+          command = "vendor/bin/pint",
+          args = { "--path", "$FILENAME" },
+        }),
+
+
 
         --require("none-ls.formatting.biome"),
         --require("none-ls.diagnostics.biome"),
