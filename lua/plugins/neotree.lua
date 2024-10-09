583f6c113424fc6cfdbb7caa86ade29ae7a6ae7c
BrunoPoianoVlcode <bruno.poiano@vlcode.com.br>
2024-10-09 19:19:06 -0300

    update keybindings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,12 +7,21 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>', {})
-    vim.keymap.set('n', '<C-n>', ':Neotree close<CR>', {})
+    vim.keymap.set('n', '<leader>b', function()
+      require('neo-tree.command').execute({ toggle = true })
+    end, { noremap = true, silent = true })
+
     require("neo-tree").setup({
       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
       popup_border_style = "rounded",
       enable_git_status = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
       event_handlers = {
         {
           event = "file_open_requested",
